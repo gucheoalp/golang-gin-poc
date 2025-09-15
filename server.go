ee246951f8126834b7eaf3fba82e31c95c3f548f
Godwin Uche <guche@olaniwunajayi.net>
2025-09-15 10:58:17 +0100

    Working with middlewares

--- server.go
+++ server.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gucheoalp/golang-gin-poc/controller"
+	"github.com/gucheoalp/golang-gin-poc/middlewares"
 	"github.com/gucheoalp/golang-gin-poc/service"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var(
 	videoService service.VideoService = service.New()
 	videoController controller.VideoController = controller.New(videoService)
 )
+
+func setupLogOutput() {
+	f, err := os.Create("gin.log")
+	if err != nil {
+		panic(err)
+	}
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	gin.DefaultErrorWriter = gin.DefaultWriter
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+
+	server := gin.New()
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/posts", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
