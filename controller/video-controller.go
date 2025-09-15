3dd773ef37e8bd9511632f9332cab6e9fb00fae2
Godwin Uche <guche@olaniwunajayi.net>
2025-09-15 13:33:11 +0100

    Validation and Binding in GoLang

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -2,13 +2,16 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"github.com/gucheoalp/golang-gin-poc/entity"
 	"github.com/gucheoalp/golang-gin-poc/service"
+	"github.com/gucheoalp/golang-gin-poc/validators"
 )
 
 type VideoController interface {
 	FindAll() []entity.Video
-	Save(ctx *gin.Context) entity.Video
+	Save(ctx *gin.Context) error
 }
 
 type controller struct {
@@ -16,6 +19,11 @@ type controller struct {
 }
 
 func New(service service.VideoService) VideoController {
+	// Register custom validator with Gin's validator instance
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("is-cool", validators.ValidateCoolTitle)
+	}
+	
 	return &controller{
 		service: service,
 	}
@@ -25,9 +33,15 @@ func (c *controller) FindAll() []entity.Video  {
 	return c.service.FindAll()
 }
 
-func (c *controller) Save(ctx *gin.Context) entity.Video {
+func (c *controller) Save(ctx *gin.Context) error {
 	var video entity.Video
-	ctx.BindJSON(&video)
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		// Return a proper error response to the client
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return err
+	}
+	
 	c.service.Save(video)
-	return video
+	return nil
 }
--- server.go
+++ server.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gucheoalp/golang-gin-poc/controller"
 	"github.com/gucheoalp/golang-gin-poc/middlewares"
 	"github.com/gucheoalp/golang-gin-poc/service"
-	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var(
@@ -29,13 +29,18 @@ func main() {
 	setupLogOutput()
 
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
 
 	server.GET("/posts", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is VALID!"})
+		}
 	})
 
 	server.Run(":8091")
